8f8548d26a0f0eb77f8404bf765af4c9d8879b1a
Yegor Bugayenko <yegor256@gmail.com>
2025-01-29 11:20:34 +0300

    files for codecov

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-cd $(dirname $0)
+cd "$(dirname "$0")"
 cp /code/home/assets/rehttp/settings.xml .
 git add settings.xml
 git commit -m 'settings.xml for heroku'
 trap 'git reset HEAD~1 && rm settings.xml' EXIT
 git push heroku master -f
-
