fe5c1641cc9ddac37577364a1a8e95bad4f73cd9
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-11-25 11:46:59 +0300

    Decorator fixes

--- src/main/java/net/rehttp/tk/TkNotFoundSafeDetails.java
+++ src/main/java/net/rehttp/tk/TkNotFoundSafeDetails.java
@@ -5,20 +5,12 @@
 package net.rehttp.tk;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import net.rehttp.base.Base;
-import org.cactoos.list.ListOf;
 import org.takes.Request;
 import org.takes.Response;
 import org.takes.Take;
-import org.takes.rq.RqHref;
-import org.takes.rs.xe.XeDirectives;
-import org.xembly.Directive;
 
 /**
- * Details of request.
+ * Decorator for handle not exist records.
  *
  * @since 1.0
  */
@@ -40,7 +32,7 @@ final class TkNotFoundSafeDetails implements Take {
     @Override
     public Response act(final Request req) throws IOException {
         try {
-            this.origin.act(req);
+            return this.origin.act(req);
         } catch (final IOException ex) {
             return RsPage(
                 "/xsl/not_found.xsl",
